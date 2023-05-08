e0bee74a57d2f7ab7e847e449edfbc0e09a70ae3
Matthieu Beauchamp-B <Matthieu_927@hotmail.ca>
2023-05-08 14:16:38 -0400

    Fixed submodule update internet check and added coverage-viewer target

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -9,50 +9,18 @@ macro(simu_define_option var default type docstring)
     set(${var} ${${var}} CACHE ${type} ${docstring} FORCE)
 endmacro()
 
-function(simu_has_internet boolOut)
-    # Avoids CMake configuration failures due to fetchContent when 
-    #   not connected to internet
-    
-    # https://stackoverflow.com/a/62233098
-    if (MSVC)
-        execute_process(
-            COMMAND ping www.google.com /n 2 /w 1000
-            ERROR_QUIET
-            OUTPUT_QUIET
-            RESULT_VARIABLE NO_CONNECTION
-        )
-    else()
-        execute_process(
-            COMMAND ping www.google.com -c 2 -w 1000
-            ERROR_QUIET
-            OUTPUT_QUIET
-            RESULT_VARIABLE NO_CONNECTION
-        )
-    endif()
-
-    if (NO_CONNECTION GREATER 0)
-        set(${boolOut} FALSE PARENT_SCOPE)
-    else()
-        set(${boolOut} TRUE PARENT_SCOPE)
-    endif()
-
-    return()
-endfunction()
-
 function(simu_update_submodules dir)
     find_package(Git)
 
     if (${Git_FOUND})
-        # TODO: github workflows does not allow us to ping, causing this to not run
-        # simu_has_internet(hasInternet)
-        # if (${hasInternet})
-            execute_process(
-                COMMAND "${GIT_EXECUTABLE}" submodule update --init --recursive .
-                WORKING_DIRECTORY "${dir}"
-            )
-        # else()
-        #     message(WARNING "Not connected to internet, cannot update submodules")
-        # endif()
+        execute_process(
+            COMMAND "${GIT_EXECUTABLE}" submodule update --init --recursive .
+            WORKING_DIRECTORY "${dir}"
+            RESULT_VARIABLE status
+        )
+        if(status AND NOT status EQUAL 0)
+            message(STATUS "Could not update submodules: ${status}")
+        endif()
     else()
         message(WARNING "Could not find git, cannot update submodules")
     endif()
@@ -90,6 +58,11 @@ macro(simu_coverage targetName)
         setup_target_for_coverage_lcov(NAME ${targetName}-coverage 
             EXECUTABLE ${targetName}
             EXCLUDE "test/*" "*/catch2/*"
+        )
+
+        add_custom_target(${targetName}-coverage-viewer
+            COMMAND firefox ${PROJECT_BINARY_DIR}/${targetName}-coverage/index.html
+            DEPENDS ${targetName}-coverage
         )
     endif()
 endmacro()
