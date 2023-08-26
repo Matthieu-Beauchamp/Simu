669c186934ca3e7aaebb89be1b3abb7f3cf5d315
Matthieu Beauchamp-B <Matthieu_927@hotmail.ca>
2023-08-26 14:36:54 -0400

    Basic idea for a possibly better PolymorphicList, goal is to remove the overhead from the custom deleters of our UniquePtr

--- include/Simu/utility/PolymorphicList.hpp
+++ include/Simu/utility/PolymorphicList.hpp
@@ -32,8 +32,104 @@
 namespace simu
 {
 
-template <class T, class Alloc>
-using PolymorphicList = std::vector<UniquePtr<T>, ReboundTo<Alloc, UniquePtr<T>>>;
+// template <class T, class Alloc>
+// using PolymorphicList = std::vector<UniquePtr<T>, ReboundTo<Alloc, UniquePtr<T>>>;
+
+template <class T, class Alloc_>
+class PolymorphicList
+{
+public:
+
+private:
+
+    typedef Uint8          Index;
+    static constexpr Index maxIndex = std::numeric_limits<Index>::max();
+
+    template <class U>
+    using AllocatorFor = ReboundTo<Alloc_, PolyNode<U>>;
+
+    struct Node
+    {
+        Node* next = nullptr;
+        Node* prev = nullptr;
+        Index type;
+    }
+
+    template <std::derived_from<T> U>
+    struct PolyNode : public Node
+    {
+        U data;
+    };
+
+    struct VTable
+    {
+        template <class U>
+        static void destructorForU(const Alloc_& alloc, Node* node)
+        {
+            auto derivedNode = static_cast<PolyNode<U>*>(node);
+
+            std::allocator_traits<AllocatorFor<U>>::destroy(alloc, derivedNode);
+            std::allocator_traits<AllocatorFor<U>>::deallocate(alloc, derivedNode);
+        }
+
+        // is this needed?
+        // I believe it is necessary in case we have:
+        // class U : SomeClass, T
+        // -> U* != T*
+        template <class U>
+        static T* castForU(Node* node)
+        {
+            auto derivedNode = static_cast<PolyNode<U>*>(node);
+            return static_cast<T*>(&derivedNode->data);
+        }
+
+        template <class U>
+        static VTable makeVTable()
+        {
+            VTable table;
+            table.destructor = destructorForU<U>;
+            table.cast       = castForU<U>;
+        }
+
+
+        typedef void (*Destructor)(const Alloc_& alloc, Node*);
+        Destructor destructor;
+
+        typedef T* (*Cast)(Node*);
+        Cast cast;
+    };
+
+    template <class U>
+    PolyNode<U>* allocate()
+    {
+        static Index tableIndex = maxIndex;
+
+        bool isFirstCall = tableIndex == maxIndex;
+        if (isFirstCall)
+        {
+            SIMU_ASSERT(
+                nTables_ < maxIndex, "Too many classes in this polymorphic list"
+            );
+
+            tableIndex          = nTables_++;
+            tables_[tableIndex] = VTable::makeVTable<U>();
+        }
+
+        PolyNode<U>* polyNode = std::allocator_traits<AllocatorFor<U>>::allocate(
+            alloc_, 1
+        );
+        polyNode->type = tableIndex;
+        return polyNode;
+    }
+
+    void erase(Node* node) { tables_[node->type].destructor(alloc_, node); }
+
+    static std::array<VTable, maxIndex> tables_;
+    static Index                        nTables_ = 0;
+
+
+    Alloc_ alloc_;
+};
 
 
 } // namespace simu
